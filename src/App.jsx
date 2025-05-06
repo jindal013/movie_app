afe649d5b8601cdf1883b360a26afef914e2c644
Chinmay <chinmayjindal108@gmail.com>
2025-05-06 19:59:00 -0400

    use effect dependency

--- src/App.jsx
+++ src/App.jsx
@@ -4,20 +4,22 @@ const Card = ({ title }) => {
   const [count, setCount] = useState(0);
   const [hasLiked, setHasLiked] = useState(false);
 
-  useEffect( () => {
-    console.log(`${title} has been liked: ${hasLiked}`)
+  useEffect(() => {
+    console.log(`${title} has been liked: ${hasLiked}`);
   }, [hasLiked]);
 
   useEffect(() => {
-    console.log('card rendered!');
+    console.log("card rendered!");
   });
 
   return (
-    <div className="card" onClick={ () => setCount(c => c + 1)}>
-      <h2>{title} <br/> {count || null }</h2>
+    <div className="card" onClick={() => setCount((c) => c + 1)}>
+      <h2>
+        {title} <br /> {count || null}
+      </h2>
 
-      <button onClick={ () => setHasLiked(!hasLiked)}>
-        {hasLiked ? '❤️' : '🤍'}
+      <button onClick={() => setHasLiked(!hasLiked)}>
+        {hasLiked ? "❤️" : "🤍"}
       </button>
     </div>
   );
@@ -26,11 +28,9 @@ const Card = ({ title }) => {
 const title = "react";
 
 const App = () => {
-
-
   return (
     <div className="card-container">
-      <Card title="Star Wars" rating={5} isCool={true}/>
+      <Card title="Star Wars" rating={5} isCool={true} />
       <Card title="Lion King" />
       <Card title="Avatar" />
     </div>
